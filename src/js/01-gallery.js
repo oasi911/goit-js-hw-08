3ed86121039821aba4dc035905b3616f8e63e8c4
oasi911 <sokolanton.1998@gmail.com>
2023-03-20 21:48:02 +0200

    init

---
+++ src/js/01-gallery.js
@@ -0,0 +1,28 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+import SimpleLightbox from '../../';
+
+const gallery = document.querySelector('ul.gallery');
+
+function createGallery(galleryItems) {
+  const images = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li><a class="gallery__item simpleLightbox" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a></li>`;
+    })
+    .join('');
+  return images;
+}
+
+gallery.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+new SimpleLightbox('.simpleLightbox', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
