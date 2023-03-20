abe9ce6ffeed0198d886c8a6e401488c06087bab
oasi911 <sokolanton.1998@gmail.com>
2023-03-20 23:18:07 +0200

    tast 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,13 +2,14 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('ul.gallery');
 
 function createGallery(galleryItems) {
   const images = galleryItems
     .map(({ preview, original, description }) => {
-      return `<li><a class="gallery__item simpleLightbox" href="${original}">
+      return `<li style="list-style:none;"><a class="gallery__item simpleLightbox" href="${original}">
           <img
             class="gallery__image"
             src="${preview}"
