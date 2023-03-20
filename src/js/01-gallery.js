f80e395392ec59df9aff287f0a3498177717e71b
oasi911 <sokolanton.1998@gmail.com>
2023-03-20 21:50:47 +0200

    v0.1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,7 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-import SimpleLightbox from '../../';
+import SimpleLightbox from 'simplelightbox';
 
 const gallery = document.querySelector('ul.gallery');
 
